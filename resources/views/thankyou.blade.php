643d3f2ace9a47e6aad4605eff9babd9f8ae1132
ntds <nkndmsts24@gmail.com>
2019-12-15 10:57:30 +0000

    thankyouページ修正

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -2,5 +2,7 @@
 
 @section('content')
     <div class="text-center">
-            <h1>ご利用ありがとうございました！</h1>
+        <h1>ご利用ありがとうございました。<br>
+            投稿の閲覧はどなたでもできるので、そちらはご利用ください！
+        </h1>
 @endsection
